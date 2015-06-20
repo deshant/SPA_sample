20896d75350a0d18df82fd5d443c9174d6ade43e
Deshant Sharma <deshant_sharma@outlook.com>
2015-06-20 21:57:11 +0530

    Working code

--- WebContent/control.js
+++ WebContent/control.js
@@ -3,9 +3,9 @@
     //
     var spApp = angular.module( "ngSpApp", []);
     
-    spApp.config(function($httpProvider){
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  });
+//    spApp.config(function($httpProvider){
+//      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+//  });
 
     //
     // Controller for Home Page
@@ -14,8 +14,8 @@
       
 	$scope.look = function(){
 		//$scope.text = 'Hello, this is homepage, under construction';
-		var ticker = $scope.symbol;
-		$scope.text = getData($scope.symbol);
+		$scope.output = getData($scope.symbol);
+		console.info($scope.output);
 		}
       
 }]);
@@ -24,12 +24,18 @@ function getData(symbol) {
     var url = "http://query.yahooapis.com/v1/public/yql";
     
     var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
-	    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
-        .done(function (data) {
+    
+	var response = $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env");
+        
+	$.when(response).done(function (data) {
+		
+		console.info(data.query.results.quote.LastTradePriceOnly);
+		$("#result").text("Bid Price: " + data.query.results.quote.LastTradePriceOnly);
         return 'Bid Price: ' + data.query.results.quote.LastTradePriceOnly;
+        
     })
         .fail(function (jqxhr, textStatus, error) {
-        var err = textStatus + ", " + error;
+        	var err = textStatus + ", " + error;
             return 'Request failed: ' + err;
     });
 }
